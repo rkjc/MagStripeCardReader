38dc75000f132e1272d80f9e3456fab6b37adfc4
rkjc <richardkjcross@gmail.com>
2017-02-10 14:06:15 -0800

    cleaned up the output and changed record extension to .log

--- src/CardReaderMain.java
+++ src/CardReaderMain.java
@@ -13,59 +13,53 @@ public class CardReaderMain {
 		// make and open attendance file with time/date in file name
 		Scanner scanner = new Scanner(System.in);
 		String timeStamp  = new SimpleDateFormat("yyyy'-'MM'-'dd'_'hh'.'mm").format(new Date());
-		String fileName = "attendanceLog_" + timeStamp + ".txt";
+		String fileName = "attendanceLog_" + timeStamp + ".log";
 		String nameStr = "";
-		HashMap<String, String> inputHash = new HashMap<String, String>(); 
-		
-		try {	
+		HashMap<String, String> inputHash = new HashMap<String, String>();
+
+		try {
 			File attendanceLog = new File(fileName);
 			attendanceLog.createNewFile();
 			FileWriter attendance = new FileWriter(attendanceLog);
-			
-			
+
+
 			System.out.println("File " + fileName + " has been created");
-			
+
 			attendance.write("Attendance log for " + timeStamp + "\r\n");
 			attendance.write("------------------------------------\r\n");
-			
+
 			boolean runScan = true;
 			while(runScan){
 				System.out.println("Please slide student ID card");
 				String inputStr = scanner.next();
+
 				if(inputStr.equals("exit")){break;}
 
-				nameStr = inputStr;
-				
+				nameStr = inputStr;  //TODO add name parser
+				System.out.println("------------------------------------\r\n");
 				if(inputHash.putIfAbsent(inputStr, nameStr) != null){
-					System.out.println(nameStr + " has already been logged");
+					System.out.println(nameStr + "\r\n\r\nhas already been logged\r\n");
 				} else {
 					attendance.write(inputStr + "\r\n");
-					System.out.println(nameStr + " hass been logged as present");
+					attendance.flush();
+					System.out.println(nameStr + "\r\n\r\nhas been logged as present\r\n");
 				}
-				
-			
+				System.out.println("------------------------------------\r\n");
 			}
-			
-        		
-		//while loop - test input for "end" or "quit"
-		//print "Please scan Student ID"
-		//check if value in hashmap
-		//if not in hashmap, parse name, add to hash, append to attendance file
-		//print name has been logged -or- name is already logged
+
 		//wait 2 sec
 		//clear screen
-		//loop while
-		//close file
-			
+
+
 			attendance.flush();
 			attendance.close();
 			System.out.println("Attendance file has been closed");
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
